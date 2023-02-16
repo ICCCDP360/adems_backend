1c5f2d4a8da183d59c06d3dc08ac25aa230fd3b9
Indhumathi <indhumathi4703@gmail.com>
2023-02-16 14:20:17 +0530

    signin page some changes

--- controller/signin.js
+++ controller/signin.js
@@ -26,39 +26,11 @@ exports.CheckAccount = async (req, res) => {
   } catch (error) {
     res.status(200).json({ message: error.message });
   }
-  // try{
-
-  //     const PostSignin= new signin(
-  //         {
-  //             user_id:reqData.user_name,
-  //             pwd:reqData.pwd
-  //         }
-  //     )
-  //     const savePostSignin=await PostSignin.save()
-  //     res.status(200).json(savePostSignin);
-  // }
-  // catch(err){
-  //     console.log(err);
-  // }
 };
 
-// Post Sch_principal
+// Post Signin
 exports.PostSignin = async (req, res) => {
   const reqData = req.body;
   console.log("req body", reqData);
   res.status(200).json({ message: "login coming soon" });
-  // try{
-
-  //     const PostSignin= new signin(
-  //         {
-  //             user_id:reqData.user_name,
-  //             pwd:reqData.pwd
-  //         }
-  //     )
-  //     const savePostSignin=await PostSignin.save()
-  //     res.status(200).json(savePostSignin);
-  // }
-  // catch(err){
-  //     console.log(err);
-  // }
 };
