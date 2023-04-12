4573b637d31b20ea937ceec9b037dd819fd7189f
vimal_ROG <vimal@cdp360.com>
2023-04-12 12:24:57 +0530

    port 8001 changed

--- config/credentials/config.js
+++ config/credentials/config.js
@@ -1,6 +1,6 @@
 const TABLE_NAME = "Adems";
 const config = {
-  PORT: process.env.PORT || 8080,
+  PORT: process.env.PORT || 8001,
   //DB_CONNECTION: `mongodb://localhost/${TABLE_NAME}`,
   DB_CONNECTION: `mongodb+srv://samdev:Password%40123@chessthambi.vllen.mongodb.net/adems_new`,
   SECRET: "ademslmscdp360",
