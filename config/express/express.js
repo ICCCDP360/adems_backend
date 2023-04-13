27304c97e0305bef6dc6c497a1d750c55e0f4720
PrakashDeveloperCDP360 <prakash@cdp360.com>
2023-04-13 17:11:56 +0530

    url added

--- config/express/express.js
+++ config/express/express.js
@@ -22,6 +22,7 @@ module.exports = (app) => {
         "https://students.goadem.com",
         "https://school.goadem.com",
         "https://school.cdp360.in",
+        "https://dev-lms-student.cdp360.in"
       ],
       credentials: true,
     })
