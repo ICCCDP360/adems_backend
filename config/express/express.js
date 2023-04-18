c372c04145bc127fcfeefbde8cc94bda534b3c25
PrakashDeveloperCDP360 <prakash@cdp360.com>
2023-04-18 13:20:44 +0530

    cors changes

--- config/express/express.js
+++ config/express/express.js
@@ -12,21 +12,22 @@ module.exports = (app) => {
   require("../mongodb/mongoose");
   app.use(corsOrgin);
   app.use(logger("dev"));
-  app.use(
-    cors({
-      origin: [
-        "http://localhost:3000",
-        "https://student.cdp360.in",
-        "https://student.goadem.com",
-        "https://students.cdp360.in",
-        "https://students.goadem.com",
-        "https://school.goadem.com",
-        "https://school.cdp360.in",
-        "https://dev-lms-student.cdp360.in"
-      ],
-      credentials: true,
-    })
-  );
+  // app.use(
+  //   cors({
+  //     origin: [
+  //       "http://localhost:3000",
+  //       "https://student.cdp360.in",
+  //       "https://student.goadem.com",
+  //       "https://students.cdp360.in",
+  //       "https://students.goadem.com",
+  //       "https://school.goadem.com",
+  //       "https://school.cdp360.in",
+  //       "https://dev-lms-student.cdp360.in"
+  //     ],
+  //     credentials: true,
+  //   })
+  // );
+  app.use('*',cors());
 
   app.use(express.json({ limit: "50mb" }));
   app.use(express.urlencoded({ limit: "50mb", extended: true }));
